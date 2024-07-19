8bb5c43e051a14a701954d4d9026c2756d8ccf64
ign-is <igna.dev.contact@gmail.com>
2024-07-19 18:09:55 -0300

    Add Guide finish version section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -21,9 +21,9 @@ const Guide = () => {
                     alt="boat"
                     width={1440}
                     height={580}
-                    className="w-full object-cover object-center 2xl:rouned-5xl"
+                    className="w-full object-cover object-center 2xl:rounded-5xl"
                 />
-                <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rouned-3xl border shadow-md md:left-[5%] lg:top-20">
+                <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
                     <Image 
                         src="/meter.svg"
                         alt="meter"
@@ -34,9 +34,15 @@ const Guide = () => {
                     <div className="flexBetween flex-col">
                         <div className="flex w-full flex-col">
                             <div className="flexBetween w-full">
-                                <p></p>
-                                <p></p>
+                                <p className="regular-16 text-gray">Destination</p>
+                                <p className="bold-16 text-green-50">48 min</p>
                             </div>
+                            <p className="bold-20 mt-2">Aguas Calientes</p>
+                        </div>
+
+                        <div className="flex w-full flex-col">
+                            <p className="regular-16 text-gray-20">Start Track</p>
+                            <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
                         </div>
                     </div>
                 </div>
