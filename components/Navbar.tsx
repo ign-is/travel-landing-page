ebf9ec6f145dcfa31e53cd8991d4ddd6ede72cee
ign-is <igna.dev.contact@gmail.com>
2024-07-21 20:51:58 -0300

    Add Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="flexBetween max-container padding-container relative z-30 py-5">
             <Link href="/">
-                <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+                <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} className="w-full h-auto"/>
             </Link>
 
             <ul className="hidden h-full gap-12 lg:flex">
